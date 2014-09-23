bd683c17d4aee9f495e0c696591ebc609e0d46c5
Toni Harju <toni.harju@cou.fi>
2014-09-23 21:31:04 +0300

    - Added initial tile placement - Added initial camera movement (wasd, scroll to zoom)

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	public float Velocity = 10;
+	public float ZoomVelocity = 100;
+
+	// Use this for initialization
+	void Start () {
+	
+		transform.position = new Vector3( 0, 5, 0 );
+		transform.rotation = Quaternion.Euler ( 45, 315, 0 );
+
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+	
+		float horizontal = Input.GetAxis ( "Horizontal" );
+		float vertical = Input.GetAxis ( "Vertical" );
+		float scroll = Input.GetAxis ( "Mouse ScrollWheel" );
+
+		if( vertical != 0 ) {
+
+			transform.localPosition = transform.localPosition + ( Quaternion.Euler( 0, -45, 0 ) * new Vector3( 0, 0, vertical * Time.deltaTime * Velocity ) );
+
+		}
+
+		if( horizontal != 0 ) {
+			
+			transform.localPosition = transform.localPosition + ( Quaternion.Euler( 0, -45, 0 ) * new Vector3( horizontal * Time.deltaTime * Velocity, 0 ) );
+			
+		}
+
+		if( scroll != 0 ) {
+
+			transform.Translate ( 0, 0, scroll * Time.deltaTime * ZoomVelocity );
+
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum TileType {
+
+	Grass
+
+}
+
+public class Tile : MonoBehaviour {
+
+	private MeshFilter meshFilter;
+	private MeshRenderer meshRenderer;
+
+	public TileType Model;
+
+	public Vector2 Position;
+
+	// Use this for initialization
+	void Start () {
+	
+		transform.rotation = Quaternion.Euler ( 270, 180, 0 );
+		transform.localScale = new Vector3( 100, 100, 100 );
+
+		gameObject.AddComponent< MeshFilter >();
+		gameObject.AddComponent< MeshRenderer >();
+
+		meshFilter = GetComponent< MeshFilter >();
+		meshRenderer = GetComponent< MeshRenderer >();
+	
+		switch( Model ) {
+
+			case TileType.Grass:
+
+				Mesh sharedMesh = Resources.Load< Mesh >( "Models/HexagonGrass" );
+				Material[] sharedMaterials = new Material[2];
+				sharedMaterials[0] = Resources.Load< Material >( "Materials/HexagonGrass-SidesMaterial" );
+				sharedMaterials[1] = Resources.Load< Material >( "Materials/HexagonGrass-TopMaterial" );
+
+				meshFilter.mesh = sharedMesh;
+				meshRenderer.materials = sharedMaterials;
+
+				break;
+
+		}
+
+		if( Position.x % 2 == 1 ) 
+			transform.position = new Vector3( Position.x * 1.5f, 0, -1 * Position.y - 1 - Position.y );
+		else
+			transform.position = new Vector3( Position.x * 1.5f, 0, -1 * Position.y * 2 );
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
