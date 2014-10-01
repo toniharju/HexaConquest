89e08ef90bf4f4999dc960c16fd4ad7c8641957f
Toni Harju <toni.harju@cou.fi>
2014-10-01 10:35:11 +0300

    - Modified tile selection visuals - Escape and clicking "out-of-bounds" deselects a selected tile - Added a directional light

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -4,30 +4,57 @@ using System.Collections;
 public class TileManager : MonoBehaviour {
 
 	private GameObject current;
+	private Color[] originalColor;
 
 	// Use this for initialization
 	void Start () {
 	
-
+		originalColor = new Color[2];
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+		if ( current != null && Input.GetKeyDown ( KeyCode.Escape ) ) {
+
+			current.renderer.materials[0].color = originalColor[0];
+			current.renderer.materials[1].color = originalColor[1];
+
+			current = null;
+
+		}
+
 		if ( Input.GetMouseButtonUp ( 0 ) ) {
 			
 			RaycastHit hit;
 			
 			if ( Physics.Raycast ( Camera.main.ScreenPointToRay ( Input.mousePosition ), out hit ) ) {
 
-				if( current != null )
-					current.renderer.material.color = new Color( 0, 0, 0 );
+				if( current != null ) {
+					current.renderer.materials[0].color = originalColor[0];
+					current.renderer.materials[1].color = originalColor[1];
+				}
 
 				current = hit.transform.gameObject;
 
-				current.renderer.material.color = new Color( 0, 255, 0 );
+				originalColor[0] = current.renderer.materials[0].color;
+				originalColor[1] = current.renderer.materials[1].color;
+
+				current.renderer.materials[0].color = new Color( 0.0f, 1.0f, 0.0f );
+				current.renderer.materials[1].color = new Color( 0.0f, 1.0f, 0.0f );
 				
+			} else {
+
+				if ( current != null ) {
+					
+					current.renderer.materials[0].color = originalColor[0];
+					current.renderer.materials[1].color = originalColor[1];
+					
+					current = null;
+					
+				}
+
 			}
 			
 		}
