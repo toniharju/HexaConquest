ac03c995cad683dd1c3db4ae954d3b8ae423bdcd
Jan Ågren <jan.agren@cou.fi>
2014-10-08 14:43:01 +0300

    Modified mechanics

--- Assets/Scripts/Mechanics.cs
+++ Assets/Scripts/Mechanics.cs
@@ -10,59 +10,61 @@ public class Mechanics : MonoBehaviour {
 	public bool activeTurn = false;
 
 	//establish D4- and D6-dices
-	public int d4 = Random.Range(1,4);
+	public int d3 = Random.Range(1,3);
 	public int d6 = Random.Range(1,6);
+	public int hitDice;
+
 	
 	
 	//Power
 	public int meleePower;
 	public int rangedPower;
 	public int horsePower;
-	public int attackPower;
-	public int defencePower;
 	
 	//Healths
 	public int meleeHealth = 10;
 	public int rangedHealth = 5;
-	public int horseHealth = 15;
-	public int totalHealth;
+	public int horseHealth = 20;
 	
 	//Unit count
 	public int meleeCount = 0;
 	public int rangedCount = 0;
 	public int horseCount = 0;
+	public int totalCount = meleeCount + rangedCount + horseCount;
+	public int maxCount = 16;
 
 	void Start () {
 
 		activeTurn = true;
-	
-
-		//Health + total health
-
-		meleeHealth = meleeHealth * meleeCount;
-		rangedHealth = rangedHealth * rangedCount;
-		horseHealth = horseHealth * horseCount;
-		totalHealth = meleeHealth + rangedHealth + horseHealth;
+		hitDice = Random.Range (1, totalCount);
 
 
+
+
+		//Check that the tile's unit count isn't greater than the max unit count
+
+		if (totalCount > maxCount) {
+			/*Placeholder for A HUUUUUGE warning sign, that says
+			 * "Fuck off, we are full!!!"
+			 * 
+			 * 
+			 * */
+				} else {
+			/* Placeholder for succesful check
+			 * 
+			 * 
+			 * */
+
+
+				}
 
 		//Power calculation for active turn (Attacker)
 		if (activeTurn == true){
 
-			meleePower = (10 + d4)*meleeCount;
-			rangedPower = (15 + d4)*rangedCount;
-			horsePower =( 20 + d4)*horseCount;
-			attackPower = meleePower + rangedPower + horsePower;
+			meleePower = (10 + d3)-d6;
+			rangedPower = (15 + d3)-d6;
+			horsePower =(20 + d3)-d6;
 		} 
-
-		//Power calculation for non-active turn (Defender)
-	else {
-
-			meleePower = (10 + d6)*meleeCount;
-			rangedPower = (15 + d6)*rangedCount;
-			horsePower = (20 + d6)*horseCount;
-			defencePower = meleePower + rangedPower + horsePower;
-		}
 
 
 	
